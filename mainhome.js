435eb57589686e0f96135ee0a659aaef245f0dd4
Prekshabhura <49183294+Prekshabhura@users.noreply.github.com>
2021-01-31 10:50:32 +0530

    Add files via upload

---
+++ mainhome.js
@@ -0,0 +1,15 @@
+function navigate(){
+    window.location="Bedroom.html";
+}
+function navigate2(){
+    window.location="Books.html";
+}
+function navigate3(){
+    window.location="desk.html";
+}
+function navigate4(){
+    window.location="bottle.html";
+}
+function navigate5(){
+    window.location="plates.html";
+}
